ab1d3f7de468236ca979bbc9d057c20e8cbad7ea
skschaff <skschaffner@gmail.com>
2024-06-06 19:04:03 -0400

    Program for Checkpoint Exercise 2.17

--- Chapter 2 Checkpoint Exercises.cpp
+++ Chapter 2 Checkpoint Exercises.cpp
@@ -1,20 +1,21 @@
 // Chapter 2 Checkpoint Exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This main function is for the Checkpoint 2.17 program. 
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string name, address, phoneNumber;
+
+    name = "Sally Schaffner";
+    address = "243 Southlake Place\nNewport News, VA 23602";
+    phoneNumber = "757-358-2457";
+
+    cout << name << endl << address << endl << phoneNumber << endl;
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
